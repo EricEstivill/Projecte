5d1f515d60c06dcb38f98f776b5df53c603bbd98
EricEstivill <94860401+EricEstivill@users.noreply.github.com>
2021-12-13 16:21:58 +0100

    Add files via upload

--- src/Projecte/projecte.java
+++ src/Projecte/projecte.java
@@ -49,13 +49,14 @@ public class projecte {
     public static String PATHPROCESADES = "files/ENTRADES PROCESADES/";
     static Connection connectarBD = null;
     private static int MAXSTOCK;
-    public static String [] Proveidor_Array= new String[100];
-    int [] Productes_Array = new int[100];
+    public static String [] Proveidor_Array= new String[5];
+    public static int [] Productes_Array = new int[5];
     public static void main (String[] args) throws SQLException, IOException {
        
         boolean sortir=false;
         connectarBD();
         Scanner teclat = new Scanner (System.in);
+        Scanner st = new Scanner(System.in);
         
             //12/11/21
         do{
@@ -64,25 +65,28 @@ public class projecte {
            System.out.println("2.Actualitzar stocks");
            System.out.println("3.Generar comanda als proveïdors");
            System.out.println("4.Consultar comandes");
-           System.out.println("5.Sortir");
+           System.out.println("S.Sortir");
            System.out.println("\nTria una de les opcions");
-           
-           int opcio=teclat.nextInt();
-           
+            String sa = st.next();
+            char opcio = sa.charAt(0);      
+            
            switch (opcio){
-               case 1:
+               case '1':
                     gestioProductes ();
                     break;
-               case 2:
+               case '2':
                     actualitzarStocks();
                     break;
-               case 3:
+               case '3':
                     genComands2();
                     break;
-               case 4:
+               case '4':
                     consultarComandes();
                     break;
-               case 5:
+               case 's':
+                   sortir=true;
+                   break;
+                case 'S':
                    sortir=true;
                    break;
                default:
@@ -123,38 +127,39 @@ public class projecte {
     
     static void gestioProductes () throws SQLException {
         Scanner teclat = new Scanner (System.in);
+        Scanner st = new Scanner(System.in);
         boolean enrere=false;
         do{
            System.out.println("\n^^^^MENU GESTOR PRODUCTES^^^^");
            System.out.println("1.Llista Productes");
-           System.out.println("2.Llistar producte determinat");
-           System.out.println("3.Alta de Productes");
-           System.out.println("4.Modificar Productes");
-           System.out.println("5.Esborrar Productes");
+           System.out.println("2.Alta de Productes");
+           System.out.println("3.Modificar Productes");
+           System.out.println("4.Esborrar Productes");
            System.out.println("S.Enrere");
            System.out.println("\nTria una de les opcions");
 
-            int opcio=teclat.nextInt();
+            String sa = st.next();
+            char opcio = sa.charAt(0);  
             
 
 
            switch (opcio){
-                case 1:
+                case '1':
                     llistaProductes();
                     break;
-                case 2:
-                    mostrarProducteDeterminat();
-                    break;
-                case 3:
+                case '2':
                     altaProductes();
                     break;
-                case 4:
+                case '3':
                     modificarProductes();
                     break;
-                case 5:
+                case '4':
                     esborrarProductes();
                     break;
-                case 6:
+                case 's':
+                   enrere=true;
+                   break;
+                case 'S':
                    enrere=true;
                    break;
                 default:
@@ -184,67 +189,7 @@ public class projecte {
         }
     }
     
-    static void mostrarProducteDeterminat() throws SQLException {
-        boolean enrere=false;
-        
-        Scanner teclat = new Scanner (System.in);
-        Scanner st = new Scanner(System.in);
-        do {
-
-            System.out.println("com vols identificar el producte");
-            System.out.println("1. Per ID");
-            System.out.println("2. Per NOM");
-            System.out.println("s. sortir");
-            String sa = st.next();
-            char opcio2 = sa.charAt(0);
-            System.out.println("la opcio: " + opcio2);
-            switch (opcio2) {
-                case '1':
-                    System.out.println("Ficar el ID del producte:");
-                    Codi_id = teclat.nextInt();
-                    System.out.println(Codi_id);
-                    String consulta = ("select * from productes where Codi_id= " + Codi_id + ";");
-                    PreparedStatement ps = connectarBD.prepareStatement(consulta);
-                    ps.executeQuery();
-                    ResultSet rs = ps.executeQuery();
-                    while (rs.next()) {
-                        System.out.println(
-                                "Id: " + rs.getInt("Codi_id") + " Preu: " + rs.getInt("preu") + " Preu: " + " Materials: "
-                                        + rs.getString("Materials") + " Nom: " + rs.getString("nom") + " Stock: "
-                                        + rs.getInt("Stock"));
-                    }
-
-                    enrere = false;
-                    break;
-                case '2':
-                    System.out.println("Ficar el NOM del producte");
-                    teclat.nextLine();
-                    Nom = teclat.nextLine();
-                    String consulta1 = String.format("select * from productes where Nom=" + "\"%s\"" + ";", Nom);
-                    System.out.println(consulta1);
-                    PreparedStatement ps1 = connectarBD.prepareStatement(consulta1);
-                    ps1.executeQuery();
-                    ResultSet rs1 = ps1.executeQuery();
-                    while (rs1.next()) {
-                        System.out.println("id: " + rs1.getInt("Codi_id") + " Preu " + rs1.getInt("preu") + " Materials "
-                                + rs1.getString("Materials") + " nom " + rs1.getString("Nom") + " Stock "
-                                + rs1.getInt("Stock"));
-                    }
-
-                    enrere = false;
-                    break;
-
-                case 's':
-                    enrere = true;
-                    break;
-                case 'S':
-                    enrere = true;
-                    break;
-            }
-
-        } while (!enrere);
-
-    }
+   
     static void altaProductes() throws SQLException{
         Scanner teclat = new Scanner (System.in);
         String consulta = "INSERT INTO productes (Nom,Stock,Codi_prov,Materials,Descr,Preu) values(?,?,?,?,?,?)";
@@ -397,8 +342,7 @@ public class projecte {
             System.out.println("la opcio: " + opcio2);
             switch (opcio2) {
                 case '1':
-                    System.out.println("fica el nom de producte");
-                    teclat.nextLine();
+                    System.out.println("fica el nom de producte"); 
                     Nom = teclat.nextLine();
 
                     break;
@@ -576,32 +520,36 @@ public class projecte {
         PreparedStatement ps = connectarBD.prepareStatement(consulta);
         ResultSet rs = ps.executeQuery();
         
+        int contadorProveidors=0;
+        int contadorProductes=0;
         
         if (rs.next()){
-            
-            Codi_id = rs.getInt(1);
-            Nom = rs.getString(2);
-            Stock = rs.getInt(3);
             Proveidor = rs.getString(4);
+            Proveidor_Array[contadorProveidors]=rs.getString(4);
             pw = escriureCapçaleraComanda(Proveidor);
-            //int contadorProveidors=0;
-            //int contadorProductes=0;
-                   
-            //Proveidor_Array[contadorProveidors]=rs.getString(Proveidor);
-
+            
             do{
                 if(!Proveidor.equals(rs.getString(4))){
+                    Productes_Array[contadorProveidors]=contadorProductes;
+                    contadorProductes=0;
                     Proveidor=rs.getString(4);
-                   pw.close();
+                    pw.close();
+                    contadorProveidors++;
+                    Proveidor_Array[contadorProveidors]=rs.getString(4);
+
+
 
                    pw = escriureCapçaleraComanda(Proveidor);
                 }
+                contadorProductes++;
                 System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getInt(3) + "\t" + rs.getString(4));
                 pw.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getInt(3) + "\t" + rs.getString(4));
 
                
             }while(rs.next());
+            Productes_Array[contadorProveidors]=contadorProductes;
             pw.close();
+
         }
         
     }
@@ -612,12 +560,12 @@ public class projecte {
             BufferedWriter bf = new BufferedWriter(fw);
             PrintWriter pw = new PrintWriter(bf);
 //escrivim dades empresa
-                pw.println("^^^^^Electroimp^^^^^");
+                pw.println("^^^^^GAMESTOCK^^^^^");
                 pw.println("");
                 pw.println("____________________________________");
                 pw.println("Codi client: 245710");
                 pw.println("Telèfon: 973537843");
-                pw.println("Correu: electro@gmail.com");
+                pw.println("Correu: gamestock@gmail.com");
                 pw.println("____________________________________");
                 pw.println("Codi\tNom\t\tStock\tProveidor");
 //escrivim linia descriptiva
@@ -628,35 +576,55 @@ public class projecte {
     static void consultarComandes() throws SQLException{
         boolean enrere=false;
         Scanner teclat = new Scanner (System.in);
-        File carpeta=new File(PATHCOMANDES2);
-        File[] comandes=carpeta.listFiles();
-        String consulta = "SELECT prod.Stock, prov.Nom FROM productes prod, proveidor prov WHERE prod.Codi_prov=prov.Codi_pro ORDER BY codi_pro AND Stock";
+        String consulta = "SELECT Codi_pro, Nom FROM proveidor ";
         PreparedStatement ps = connectarBD.prepareStatement(consulta);
         ResultSet rs = ps.executeQuery();
-        while(rs.next()) {
-           Stock = rs.getInt(1);
-           Proveidor = rs.getString(2);
+        while (rs.next()) {
+            Codi_pro = rs.getInt(1);
+            Nom = rs.getString(2);
+        }
+        int min=Productes_Array[0];
+        int max=Productes_Array[0];
+        double med=0;
+        int y=0;
+        int a=0;
+        
+        for (int i=0;i<Proveidor_Array.length;i++){
+            System.out.println("Proveidors: " + Proveidor_Array[i] + "  Productes: " + Productes_Array[i]);
+            
         }
         
-        if (comandes == null || comandes.length == 0){
-            System.out.println("No hi han comandes creades");
-            return;
-        }else{  
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            for(int i=0; i<comandes.length; i++){
-               
-                File comanda = comandes[i];
-                System.out.println(String.format("%s (%s) - %s",
-                comanda.getName(),
-                comanda.isDirectory() ? "Carpeta" : "Arxiu" ,
-                sdf.format(comanda.lastModified())
-                ));
-                
+        for(int i=0;i<Productes_Array.length;i++){
+            if(Productes_Array[i]<min){
+            min=Productes_Array[i];
+            a=i;
+            }   
+        }System.out.println("El proveidor: " + Proveidor_Array[a] + " és el proveidor amb menys productes amb un total de: " + min + " productes");
+        
+        for(int i=0;i<Productes_Array.length;i++){
+            if(Productes_Array[i]>max){
+            max=Productes_Array[i];
+            y=i;
             }
-            System.out.println("\nHi han " + comandes.length + " comandes creades");
-        }
-        }
-               
+        }System.out.println("El proveidor " + Proveidor_Array[y] + " és el proveidor amb més productes amb un total de: " + max + " productes ");
         
-        
-    }
+        for(int i=0;i<Productes_Array.length;i++){
+            med = med + Productes_Array[i];
+            
+        }med = med/Proveidor_Array.length;
+        System.out.println("Productes Mitja : " + med);
+           
+    }     
+}
+
+    
+    
+  
+    
+   
+
+ 
+
+ 
+
+          
